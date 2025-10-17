61f56c8b57f1e85fcf1d2d710c66d9e4643303ee
Patrick Duncan BUENAVENTURA <Patrick.BUENAVENTURA@student.tafesa.edu.au>
2025-10-18 00:56:44 +0800

    Add CV and asset images for portfolio

---
+++ src/assets/assets.js
@@ -0,0 +1,37 @@
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
+import profile from "../assets/profile.png";
+import fb from "../assets/fb.png";
+import insta from "../assets/insta.png";
+import html from "../assets/html-5.png";
+import css from "../assets/css-3.png";
+import js from "../assets/js.png";
+import react from "../assets/react.png";
+import php from "../assets/php.png";
+import laravel from "../assets/laravel.png";
+import mysql from "../assets/mysql.png";
+import postman from "../assets/postman.png";
+import call from "../assets/call.png";
+import mail from "../assets/mail.png";
+import location from "../assets/location.png";
+
+const asset = {
+  github,
+  linkedin,
+  profile,
+  fb,
+  insta,
+  html,
+  css,
+  js,
+  react,
+  php,
+  laravel,
+  mysql,
+  postman,
+  call,
+  mail,
+  location,
+};
+
+export default asset;
